c57a3c2f9b66aff881497cd2ec4666f191ce17b1
Simone <sdiricco.91@gmail.com>
2023-10-08 12:28:12 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-4">
+    <div class="text-5xl font-bold mb-5">Hello from vue3 template!</div>
+    <div class="text-2xl font-bold">Features: </div>
+    <ul class="text-xl">
+      <li>Vue3 latest</li>
+      <li>Pinia</li>
+      <li>Vue Router</li>
+      <li>PrimeVue</li>
+      <li>Prime Icons</li>
+      <li>Prime Flex</li>
+    </ul>
+  </div>
+</template>
