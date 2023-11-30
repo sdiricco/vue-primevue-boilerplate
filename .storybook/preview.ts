afeca6dbf0731ffda5b78af7f139ff4b7fad12c0
Simone <sdiricco@gmail.com>
2023-11-30 15:11:21 +0100

    update and add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from "@storybook/vue3";
+
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "/node_modules/primeflex/primeflex.css";
+import 'primevue/resources/themes/saga-blue/theme.css'; 
+
+
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,54 @@
+
+
+import type { Meta, StoryObj } from '@storybook/vue3';
+import MyButton from './Button.vue';
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta = {
+  title: 'Example/Button',
+  component: MyButton,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'select', options: ['small', 'medium', 'large'] },
+    backgroundColor: { control: 'color' },
+    onClick: { action: 'clicked' },
+  },
+  args: { primary: false }, // default value
+} satisfies Meta<typeof MyButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'MyButton',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    primary: false,
+    label: 'MyButton',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    label: 'MyButton',
+    size: 'large',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    label: 'MyButton',
+    size: 'small',
+  },
+};
